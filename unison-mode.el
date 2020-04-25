d5d2a98999624e1e7c419cb7602047c911c2f75c
Dario Oddenino <branch13@gmail.com>
2020-04-25 21:14:52 +0200

    some improvements

--- unison-mode.el
+++ unison-mode.el
@@ -44,32 +44,51 @@
 
 (setq unison-font-lock-keywords
       (let* (
+             ;; Regex for identifiers
+             (i-re "[a-z_][A-Za-z_!'0-9]*")
+             (t-re "[A-Z_][A-Za-z_!'0-9]*")
+             (n-re "[A-Za-z_][A-Za-z_!'0-9]*")
+             (o-re "[!$%^&*-=\\+<>.~\\/|:]+")
+
              ;; define several categories of keywords
              (x-keywords '("type" "namespace" "use" "if" "else" "unique" "ability" "where" "match" "cases" "let" "with" "handle" "forall" "infix" "infixl" "infixr" "module"))
-             (x-types '("Float" "Nat" "Int" "Boolean" "Remote" "Text" "Optional" "Either" "Sequence" "Effect" "Request" "Doc"))
 
-             ;; generate regex strigns for each category
+             ;; generate regex strings for each category
              (x-keywords-regexp (regexp-opt x-keywords 'words))
-             (x-types-regexp (regexp-opt x-types 'words))
-             (x-type-def-regexp "type\s\\(.+?\\)\s.+")
-             (x-namespace-def-regexp "namespace\s\\(.+?\\)\s.+")
-             (x-custom-type-regexp "[^A-Za-z]\\([A-Z][A-Za-z]+\\)\s")
+             ;; (x-types-regexp (regexp-opt x-types 'words))
+
+             (x-type-def-regexp (concat "type\s\\(" t-re "\\)\s.+"))
+             (x-ability-def-regexp (concat "ability\s\\(" t-re "\\)\s.+"))
+             (x-namespace-def-regexp (concat "namespace\s\\(" n-re "\\)\s+where"))
+
              (x-namespaced-regexp "[^A-Za-z]\\([A-Za-z\\.]+\\.\\)")
+             ;; TODO fix this
+
              (x-arrow-regexp "->")
              (x-colon-regexp ":")
-             (x-func-sig-regexp "\\([A-Za-z].+\s+\\):\s+.*$")
              (x-apex-regexp "'")
              (x-esc-regexp "!")
+
+             (x-func-sig-regexp (concat "\\([" n-re ".+\s+\\):\s+.*$"))
+
+             ;; (x-func-type-regexp ".+\s:\s.+\\([A-Z]\w+\\)")
+             ;; this one needs lookbehind and lookahead
+             (x-func-type-regexp (concat ".+\s+:\s.*\\(" t-re "\\)"))
+
+             (x-type-constructor-regexp "[^A-Za-z]\\([A-Z][A-Za-z]+\\)\s*")
+
              )
              `(
-               (,x-types-regexp . font-lock-type-face)
                (,x-keywords-regexp . font-lock-keyword-face)
-               (,x-arrow-regexp . font-lock-keyword-face)
                (,x-func-sig-regexp . (1 font-lock-function-name-face))
-               (,x-namespaced-regexp . (1 font-lock-constant-face))
-               (,x-type-def-regexp . (1 font-lock-constant-face))
                (,x-namespace-def-regexp . (1 font-lock-constant-face))
-               (,x-custom-type-regexp . (1 font-lock-type-face))
+               (,x-func-type-regexp . (1 font-lock-constant-face))
+               ;; (,x-namespaced-regexp . (1 font-lock-constant-face))
+               (,x-type-def-regexp . (1 font-lock-constant-face))
+               (,x-ability-def-regexp . (1 font-lock-constant-face))
+               ;; (,x-type-constructor-regexp . (1 font-lock-type-face))
+
+               (,x-arrow-regexp . font-lock-keyword-face)
                (,x-colon-regexp . font-lock-keyword-face)
                (,x-apex-regexp . font-lock-negation-char-face)
                (,x-esc-regexp . font-lock-negation-char-face)
